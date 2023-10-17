56c5cb78bacde22b44bfb886d03920caa57835be
DjuneC <cesarrodelin@outlook.com>
2023-10-17 13:53:35 -0500

    Array (Second initialization method)

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
        // array = used to store multiple values in a single variable
 
-        String[] cars = new String[3];
-
-        cars[0] = "Camaro";
-        cars[1] = "Corvette";
-        cars[2] = "Tesla";
+        String[] cars = {"Camaro", "Corvette", "Tesla"};
 
         for(int i = 0; i < cars.length; i++){
             System.out.println(cars[i]);
