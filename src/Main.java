91aa12660f911b70e4d70b460a4e44e662a2fdae
DjuneC <cesarrodelin@outlook.com>
2023-10-17 11:30:34 -0500

    Generate random numbers

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
 
-        System.out.println("Enter side x: ");
-        float sideX = sc.nextFloat();
-
-        System.out.println("Enter side y: ");
-        float sideY = sc.nextFloat();
-
-        float result = (float) Math.sqrt((sideX * sideX) + (sideY * sideY));
-
-        System.out.println("The hypotenuse of the triangle is: " + result);
-
-        sc.close();
+        int firstRandomNumber = random.nextInt(10);
+        System.out.println(firstRandomNumber);
     }
 }
