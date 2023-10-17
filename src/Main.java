90956b97f15bd8fb4371bea97bd3765d7d6769e9
DjuneC <cesarrodelin@outlook.com>
2023-10-17 13:09:05 -0500

    Loops (while, do while, for) for part

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // while loop = executes a block of code as long as its condition remains true
+       // for loop = executes a block of code a limited amount of times
+        for(int i = 10; i >= 0; i--){
+            System.out.println(i);
+        }
 
-        Scanner sc = new Scanner(System.in);
-        String name = "";
-
-        do{
-            System.out.println("What is your name ? ");
-            name = sc.nextLine();
-        }while(name.isBlank());
-
-        System.out.println("Hello " + name);
+        System.out.println("Happy new year!");
     }
 }
