3a518a624326704bb270e0e45a7cd61b1d5e00aa
DjuneC <cesarrodelin@outlook.com>
2023-10-17 13:36:17 -0500

    Nested Loops

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       // for loop = executes a block of code a limited amount of times
-        for(int i = 10; i >= 0; i--){
-            System.out.println(i);
-        }
+       // nested loops = a loop inside a loop
 
-        System.out.println("Happy new year!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a # of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.println("Enter a # of columns: ");
+        int columns = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Enter the symbol you want to display: ");
+        char symbol = sc.next().charAt(0);
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
     }
 }
