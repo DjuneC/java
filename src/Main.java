c62ac87e7cdbbbd388dd93cdd9a516c93368e4e0
DjuneC <cesarrodelin@outlook.com>
2023-10-17 14:09:31 -0500

    2D Array (First initialization method)

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,25 @@
 public class Main {
     public static void main(String[] args) {
 
-       // array = used to store multiple values in a single variable
+       // 2D array = An array of arrays
 
-        String[] cars = {"Camaro", "Corvette", "Tesla"};
+        String[][] cars = new String[3][3];
 
-        for(int i = 0; i < cars.length; i++){
-            System.out.println(cars[i]);
+        cars[0][0] = "Camaro";
+        cars[0][1] = "Corvette";
+        cars[0][2] = "Silverado";
+        cars[1][0] = "Mustang";
+        cars[1][1] = "Ranger";
+        cars[1][2] = "F-150";
+        cars[2][0] = "Ferrari";
+        cars[2][1] = "Lamborghini";
+        cars[2][2] = "Tesla";
+
+        for (int i = 0; i < cars.length; i++){
+            for (int j = 0; j < cars[i].length; j++){
+                System.out.print(cars[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
