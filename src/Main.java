05310e6ae0a0591e9523275e85653c5324b233c7
DjuneC <cesarrodelin@outlook.com>
2023-10-17 13:49:44 -0500

    Array (First initialization method)

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,17 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       // nested loops = a loop inside a loop
 
-        Scanner sc = new Scanner(System.in);
+       // array = used to store multiple values in a single variable
 
-        System.out.println("Enter a # of rows: ");
-        int rows = sc.nextInt();
+        String[] cars = new String[3];
 
-        System.out.println("Enter a # of columns: ");
-        int columns = sc.nextInt();
+        cars[0] = "Camaro";
+        cars[1] = "Corvette";
+        cars[2] = "Tesla";
 
-        sc.nextLine();
-
-        System.out.println("Enter the symbol you want to display: ");
-        char symbol = sc.next().charAt(0);
-
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < columns; j++){
-                System.out.print(symbol);
-            }
-            System.out.println();
+        for(int i = 0; i < cars.length; i++){
+            System.out.println(cars[i]);
         }
     }
 }
