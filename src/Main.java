39008e013f9b5812d3c7595f93847fcefbeca2fd
DjuneC <cesarrodelin@outlook.com>
2023-10-18 10:43:14 -0500

    printf() -> some special things about it

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,38 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // overload methods = methods that share the same name but have different parameters
-        // method name + name = method signature
+        // printf() = an optional method to control, format and display text to the console window
+        // two arguments = format string + (object/variable/value)
+        // % [flags] [precision] [width] [conversion-character]
 
-        int x = add(12, 12, 14, 25);
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Ces";
+        int myInt = 14;
+        double myDouble = 1000;
 
-        System.out.println(x);
-    }
+        // [conversion-character]
+        System.out.printf("%b",myBoolean);
+        System.out.printf("%c",myChar);
+        System.out.printf("%s",myString);
+        System.out.printf("%d",myInt);
+        System.out.printf("%f",myDouble);
 
-    static int add(int a, int b){
-        System.out.println("This is overloaded method #1");
-        return a + b;
-    }
+        // [width]
+        // minimum number of characters to be written as output
+        System.out.printf("Hello %10s", myString);
 
-    static int add(int a, int b, int c){
-        System.out.println("This is overloaded method #2");
-        return a + b + c;
-    }
+        // [precision]
+        // sets number of digits of precision when outputting floating-point values
+        System.out.printf("You have this much money %.2f", myDouble);
 
-    static int add(int a, int b, int c, int d){
-        System.out.println("This is overloaded method #3");
-        return a + b + c + d;
+        // [flags]
+        // adds an effect to output based on the flag added to format specifier
+        // - : left-justify
+        // + : output a plus ( + ) or minus ( - ) sign for a numeric value
+        // 0 : numeric values are zero-padded
+        // , : comma grouping separator if numbers > 1000
+
+        System.out.printf("You have this much money %20f", myDouble);
     }
 }
