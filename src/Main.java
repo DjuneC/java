8ba88186b650cdd15cc939e27b3e451777d3359f
DjuneC <cesarrodelin@outlook.com>
2023-10-19 09:53:43 -0500

    OOP -> The super keyword

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        // method overriding = Declaring a method in subclass,
-        //                     which is already present in parent class.
-        //                     done so that a child class can give its own implementation
-        Dog dog = new Dog();
-        dog.speak();
+        // super = keyword refers to the superclass (parent) of an object
+        //         very similar to the "this" keyword
+
+        Hero hero = new Hero("Batman", 42, "$$$");
+        Hero hero1 = new Hero("Superman", 43, "Everything");
+
+        System.out.println(hero);
     }
 }
