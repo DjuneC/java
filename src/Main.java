065058e1921c082ac41387599b17f3bb63f5af98
DjuneC <cesarrodelin@outlook.com>
2023-10-19 13:18:11 -0500

    OOP -> Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        // abstract = abstract classes cannot be instantiated, but they can have a subclass
-        //            abstract methods are declared without an implementation
-
-        //public means it is accessible in the world project
-        //private means it is accessible only inside the class it was defined
-        //protected means it is accessible inside the class it was defined and also the subclass
-        //lastly we have the default access modifier which make it available within the package it is
+        // Encapsulation = attribute of a class will be hidden or private
+        //                 can be accessed only through methods (getters & setters)
+        //                 You should make attributes private if you don't have a reason to make them public
     }
 }
